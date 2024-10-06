6dc05a43fbfc489d324ca209ad5ed62ae8aaf328
oluwabillionz96 <goodluckreuben96@gmail.com>
2024-10-06 08:35:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,23 @@ let button = document.querySelector('button');
 let body = document.querySelector('body');
 let span = document.getElementsByTagName('span')[0];
 let input2;
+body.style.backgroundColor = localStorage.getItem('color');
+span.style.color = localStorage.getItem('color');
 button.addEventListener('click',function(){
     input2 = inputs.value;
-    body.style.backgroundColor = input2;
-    span.style.color = input2;
-    inputs.value = '';
-    
+    if(!test(input2)){
+        alert(`'${input2}' is not a valid color`)
+        input2.value = '';
+    } else{
+        localStorage.setItem('color', input2 )
+        body.style.backgroundColor = localStorage.getItem('color');
+        span.style.color = localStorage.getItem('color');
+        inputs.value = '';
+    }   
 })
+
+function test(color){
+    let tester = document.createElement('div');
+    tester.style.backgroundColor = color;
+    return tester.style.backgroundColor !== '';
+}
